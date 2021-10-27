8a9798638d746f90e28b447bb070a88e7102fee1
David Yachabach <yachabach@gmail.com>
2021-10-26 22:19:01 -0400

    Debugged first test.  Added second test for change in active sensor status sends proper Alarm status to the repository.

--- starter/catpoint-parent/Catpoint_Module/src/test/java/com/udacity/catpoint/service/SecurityServiceTest.java
+++ starter/catpoint-parent/Catpoint_Module/src/test/java/com/udacity/catpoint/service/SecurityServiceTest.java
@@ -3,6 +3,7 @@ package com.udacity.catpoint.service;
 import com.udacity.catpoint.data.AlarmStatus;
 import com.udacity.catpoint.data.ArmingStatus;
 import com.udacity.catpoint.data.SecurityRepository;
+import com.udacity.catpoint.data.Sensor;
 import com.udacity.catpoint.image.service.FakeImageService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,7 +12,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
 
@@ -29,6 +33,8 @@ class SecurityServiceTest {
     private SecurityRepository securityRepository;
     @Mock
     private FakeImageService imageService;
+    @Mock
+    private Sensor sensor;
 
     //Instantiate the security service
     @BeforeEach
@@ -36,19 +42,47 @@ class SecurityServiceTest {
         securityService = new SecurityService(securityRepository, imageService);
     }
 
-    //Here we are going to send all possible arming statuses to the method.
-    //We will have to check for the DISARMED status as that will not return
-    //any value to the repository.
+    /*
+    1.	If alarm is armed and a sensor becomes activated, put the system into
+    pending alarm status.
+
+    Here we are going to send all possible arming statuses to the method.
+    We will have to check for the DISARMED status as that will not return
+    PENDING_ALARM to the repository.
+
+    For us to only get a PENDING_ALARM,  handleSensorActivated has to see that
+    the Armed Status in the repository is not DISARMED and that the Alarm
+    Status in the repository is NO_ALARM.
+     */
     @ParameterizedTest(name = "[{index}] Arming Status: {0} ")
-    @DisplayName("handleSensorActivated sends only PENDING_ALARM")
+    @DisplayName("changeSensorActivationStatus sends only PENDING_ALARM")
     @EnumSource(ArmingStatus.class)
-    void handleSensorActivated_sends_PENDING_ALARM (ArmingStatus armingStatus) {
+    void changeSensorActivationStatus_sends_PENDING_ALARM (ArmingStatus armingStatus) {
 
         if (armingStatus != ArmingStatus.DISARMED) {
-            when(securityRepository.getAlarmStatus())
-            verify(securityRepository.setAlarmStatus(), AlarmStatus.PENDING_ALARM)
 
+            Mockito.when(sensor.getActive()).thenReturn(false);
+            Mockito.when(securityRepository.getArmingStatus()).thenReturn(armingStatus);
+            Mockito.when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.NO_ALARM);
+
+            securityService.changeSensorActivationStatus(sensor, true);
+            verify(securityRepository).setAlarmStatus(eq(AlarmStatus.PENDING_ALARM));
         }
+    }
 
+    @ParameterizedTest(name = "[{index}] Arming Status: {0} ")
+    @DisplayName("changeSensorActivationStatus sets off ALARM")
+    @EnumSource(ArmingStatus.class)
+    void changeSensorActivationStatus_sends_ALARM (ArmingStatus armingStatus) {
+
+        if (armingStatus != ArmingStatus.DISARMED) {
+
+            Mockito.when(sensor.getActive()).thenReturn(false);
+            Mockito.when(securityRepository.getArmingStatus()).thenReturn(armingStatus);
+            Mockito.when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.PENDING_ALARM);
+
+            securityService.changeSensorActivationStatus(sensor, true);
+            verify(securityRepository).setAlarmStatus(eq(AlarmStatus.ALARM));
+        }
     }
 }
